469c336ba52d754d041f85578dddc28ac0892825
KarlMarxLendario <110194151+GabrielDz27@users.noreply.github.com>
2023-04-04 15:06:51 -0300

    diretivas de exemplo

--- projeto-angular-1/src/app/diretivas/diretivas.component.ts
+++ projeto-angular-1/src/app/diretivas/diretivas.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-diretivas',
   templateUrl: './diretivas.component.html',
   styleUrls: ['./diretivas.component.scss']
 })
-export class DiretivasComponent {
-
+export class DiretivasComponent implements OnInit {
+  cursos: string[] = []
+  mostrarCursos : boolean = false;
+  constructor() { }
+  ngOnInit(): void {
+  }
+  onMostrarCursos() {
+  this.mostrarCursos = !this.mostrarCursos;
+  }
 }
