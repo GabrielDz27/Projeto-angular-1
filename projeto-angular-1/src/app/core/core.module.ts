a738cb4ebf3145d81d63bb06e434e869fd944a91
KarlMarxLendario <110194151+GabrielDz27@users.noreply.github.com>
2023-04-18 15:11:24 -0300

    resolvendo alguns problemas

---
+++ projeto-angular-1/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeardComponent } from './heard/heard.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
